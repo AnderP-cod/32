936c10dbcae584dc5b90a7bb6f8c8df3ecf210b9
AnderP-cod <nemo001leyn@gmail.com>
2022-05-16 15:45:53 +0200

    Update l_32.py

--- l_32.py
+++ l_32.py
@@ -7,19 +7,27 @@ from leson_32 import examination,mistake
 from l32 import numbers1,numbers2
 
 a = numbers1()
-b = numbers2()
+try:
+    b = numbers2()
+    try:
+        item = input("Выберите знак + , - , / , *: ")
 
-item = input("Выберите знак + , - , / , *: ")
+        if item == "+":
+            print(a + b)
 
-if item == "+":
-    print(a + b)
+        elif item == "-":
+            print(a - b)
 
-elif item == "-":
-    print(a - b)
+        elif item == "*":
+            print(a * b)
 
-elif item == "*":
-    print(a * b)
+        elif item == "/":
+            examination(a, b)
+            # mistake(a,b) - власна помилка
+        else:
+            print("Ошибка")
 
-elif item == "/":
-    examination(a,b)
-    #mistake(a,b) - власна помилка
+    except ValueError:
+        print("Ошибка")
+except ValueError:
+    print("Ошибка")
