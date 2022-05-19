c5a74f4226986cf2d968c9e9f63480119ff4939e
AnderP-cod <nemo001leyn@gmail.com>
2022-05-19 19:27:55 +0200

    Update leson_32.py

--- leson_32.py
+++ leson_32.py
@@ -1,9 +1,12 @@
-def examination(a,b):
+def examination(a, b):
+    """Проверка деление на 0"""
     try:
         print(int(a / b))
     except ZeroDivisionError:
         print("айайай делить на 0 не нельзя")
 
-def mistake(a,b):
+
+def mistake(a, b):
+    """Кастомная ошибка"""
     breakpoint()
     print(a/b)
