f695307b62d3522fa41e5e6a1cbadded6d54a19b
AnderP-cod <nemo001leyn@gmail.com>
2022-05-30 15:27:29 +0200

    Update leson_32.py

--- leson_32.py
+++ leson_32.py
@@ -1,3 +1,14 @@
+class MycastomError(Exception):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        if self.b == 0:
+            print("айайай делить на 0 не нельзя")
+
+
+
 def examination(a, b):
     """Проверка деление на 0"""
     try:
@@ -5,8 +16,3 @@ def examination(a, b):
     except ZeroDivisionError:
         print("айайай делить на 0 не нельзя")
 
-
-def mistake(a, b):
-    """Кастомная ошибка"""
-    breakpoint()
-    print(a/b)
